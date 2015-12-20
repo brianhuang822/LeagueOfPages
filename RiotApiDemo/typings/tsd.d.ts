e634b29305b0613ee4471f3953f0c76723b5c77d
Daniel Son <danielsonjh@hotmail.com>
2015-12-19 19:28:21 -0800

    Downloaded request-promise.

--- RiotApiDemo/typings/tsd.d.ts
+++ RiotApiDemo/typings/tsd.d.ts
@@ -8,3 +8,7 @@
 /// <reference path="cookie-parser/cookie-parser.d.ts" />
 /// <reference path="body-parser/body-parser.d.ts" />
 /// <reference path="debug/debug.d.ts" />
+/// <reference path="bluebird/bluebird.d.ts" />
+/// <reference path="form-data/form-data.d.ts" />
+/// <reference path="request-promise/request-promise.d.ts" />
+/// <reference path="request/request.d.ts" />
